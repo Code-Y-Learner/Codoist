cf5f9c65f73330ea275d45a57639203f6887dc0b
Code D Learning <jktmdwo@naver.com>
2022-09-06 09:51:13 +0900

    make topmost

--- Python_exercise/pomodoro-start/main.py
+++ Python_exercise/pomodoro-start/main.py
@@ -83,11 +83,12 @@ def count_down(count):
 
 window = Tk()
 window.title('Pomodoro')
+window.wm_attributes("-topmost", 1)
 window.config(padx=100,pady=50,bg=YELLOW)
 
 #canvas
 canvas = Canvas(width=200,height=224,bg=YELLOW, highlightthickness=0)
-tomato_img = PhotoImage(file='./tomato.png')
+tomato_img = PhotoImage(file='D:/IT/pythonProject/Python_exercise/pomodoro-start/tomato.png')
 canvas.create_image(100,112,image=tomato_img)
 timer_text = canvas.create_text(100,130,text='00:00',fill='white',font=(FONT_NAME,35,'bold'))
 canvas.grid(column=1,row=1)
